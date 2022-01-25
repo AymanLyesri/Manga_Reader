6ef72a860cc399a4c35d9026d4e428324a37d698
ayman <lyesri99@gmail.com>
2022-01-25 13:17:37 +0100

    adjustment

---
+++ site1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>demo</title>
+</head>
+
+<body>
+    <form method="post" action="site2.php">
+        <p>login <input type="text" name="login"></p>
+        <p>password <input type="password" name="password"></p>
+        <p><input type="submit" name="send1"></p>
+    </form>
+</body>
+
+</html>
