504c0c5752eefb7e5c663833bbb9319becf806f8
ayman lyesri <lyesri99@gmail.com>
2022-05-30 21:11:04 +0100

    added detailed search with chapter selection

--- server.js
+++ server.js
@@ -9,39 +9,77 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/css", express.static("./bootstrap/css"));
 app.use("/include", express.static("./views"));
 
-//get the first page
+//get the first page////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
-//get the 'search_manga' page
+//get the 'search_manga' page///////////////////////////////////////////////////////////////////////////////////////////////
 app.get("/search_manga", (req, res) => {
   res.render("search_manga.ejs");
 });
-
-// app.post("/search_manga", (req, res) => {
-//   var manga_title = req.body.manga_title;
-//   var manga_chapter = req.body.manga_chapter;
-
-//   MFA.login("aymanthebruhman", "avalid22").then(async () => {
-//     const list = await MFA.Manga.search({
-//       title: manga_title,
-//       limit: Infinity,
-//     });
-
-//     res.render("show_manga.ejs", { list: list });
-
-//     console.log(list.length, "results for", manga_title);
-//     console.log(
-//       "The first result was written by",
-//       (await list[0].authors[0].resolve()).name
-//     );
-//   });
-// });
-
-app.post("/show_chapter", (req, res) => {
+////////////////////////////////////////////////////////////////////////////////////////////
+app.post("/search_manga", (req, res) => {
   var manga_title = req.body.manga_title;
   var manga_chapter = req.body.manga_chapter;
+
+  MFA.login("aymanthebruhman", "avalid22").then(async () => {
+    const list = await MFA.Manga.search({
+      title: manga_title,
+      limit: Infinity,
+    });
+
+    let cover = [];
+
+    for (var i = 0; i < list.length; i++) {
+      list[i].mainCover = (await list[i].mainCover.resolve()).image256;
+    }
+
+    res.render("show_manga.ejs", {
+      list: list,
+    });
+
+    console.log(list.length, "results for", manga_title);
+    console.log(
+      "The first result was written by",
+      (await list[0].authors[0].resolve()).name
+    );
+  });
+});
+////////////////////////////////////////////////////////////////////////////////////////////
+app.post("/search_chapter", (req, res) => {
+  var manga_id = req.body.manga_id;
+
+  MFA.login("aymanthebruhman", "avalid22")
+    .then(async () => {
+      console.log(manga_id);
+      // Get a manga:
+
+      let manga = await MFA.Manga.get(manga_id, true);
+      let cover = (await manga.mainCover.resolve()).image256;
+
+      // Get the manga's chapters:
+      let chapters = await manga.getFeed(
+        {
+          limit: 1000,
+          translatedLanguage: ["en"],
+        },
+        true
+      );
+      res.render("search_chapter.ejs", {
+        chapters: chapters.length,
+        manga: manga,
+        cover: cover,
+      });
+
+      // Get the first chapter's pages:
+    })
+    .catch(console.error);
+});
+////////////////////////////////////////////////////////////////////////////////////////////
+app.post("/show_chapter", (req, res) => {
+  var manga_id = req.body.manga_id;
+  var manga_chapter = parseInt(req.body.manga_chapter) - 1;
   var next_chapter = parseInt(manga_chapter) + 1;
 
   MFA.login("aymanthebruhman", "avalid22")
@@ -49,10 +87,7 @@ app.post("/show_chapter", (req, res) => {
       console.log("logged in");
       // Get a manga:
 
-      let manga = await MFA.Manga.getByQuery({
-        title: manga_title,
-        order: { createdAt: "asc" },
-      });
+      let manga = await MFA.Manga.get(manga_id);
 
       console.log(manga.year);
 
@@ -72,7 +107,7 @@ app.post("/show_chapter", (req, res) => {
 
       res.render("show_chapter.ejs", {
         panels: pages,
-        manga_title: manga_title,
+        manga_title: manga.title,
         manga_chapter: manga_chapter,
         next_chapter: next_chapter,
         manga: manga,
@@ -81,6 +116,6 @@ app.post("/show_chapter", (req, res) => {
     .catch(console.error);
 });
 
-//listen to port
+//listen to port////////////////////////////////////////////////////////////////////////////////////////////
 const port = process.env.PORT || 3000;
 app.listen(port);
