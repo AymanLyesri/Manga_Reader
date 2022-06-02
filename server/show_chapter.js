cbb0db663c531744cf696922898eeb1bb847692a
Ayman Lyesri <lyesri99@gmail.com>
2022-06-02 11:53:33 +0100

    css changes to panel display

--- server/show_chapter.js
+++ server/show_chapter.js
@@ -8,18 +8,17 @@ function showChapter(manga_id, manga_chapter, req, res) {
 
       await MFA.Manga.get(manga_id, true).then(async (manga) => {
         console.log(manga.year);
-        await manga.getFeed(
-          {
-            limit: 1000,
-            translatedLanguage: ["en"],
-            order: { chapter: "asc" },
-          },
-          true
-        );
-        then(async (chapters) => {
-          await chapters[manga_chapter]
-            .getReadablePages()
-            .then(async (pages) => {
+        await manga
+          .getFeed(
+            {
+              limit: 1000,
+              translatedLanguage: ["en"],
+              order: { chapter: "asc" },
+            },
+            true
+          )
+          .then(async (chapters) => {
+            await chapters[manga_chapter].getReadablePages().then((pages) => {
               res.render("show_chapter/show_chapter.ejs", {
                 panels: pages,
                 manga_chapter: manga_chapter,
@@ -27,7 +26,7 @@ function showChapter(manga_id, manga_chapter, req, res) {
                 manga: manga,
               });
             });
-        });
+          });
       });
     })
     .catch(console.error);
