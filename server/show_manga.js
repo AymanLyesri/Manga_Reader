903fc07cf030025e371b657be1c0ef8c9f49e486
ayman lyesri <lyesri99@gmail.com>
2022-06-06 17:17:29 +0100

    pm2 deployed, now testing image loading..

--- server/show_manga.js
+++ server/show_manga.js
@@ -1,55 +1,53 @@
 const MFA = require("mangadex-full-api"),
-  imageToBase64 = require("image-to-base64");
+    imageToBase64 = require("image-to-base64");
 
 function showManga(manga_title, offset, req, res) {
-  MFA.login("aymanthebruhman", "avalid22").then(async () => {
-    console.log("searching for " + manga_title);
-    await MFA.Manga.search(
-      {
-        title: manga_title,
-        limit: 6,
-        offset: offset,
-      },
-      false
-    )
-      .then(async (list) => {
-        console.log("manga list found");
-        if (list.length === 0) {
-          res.render("show_manga/show_manga.ejs", {
-            error: "No manga found!",
-            list: [],
-          });
-        }
+    MFA.login("aymanthebruhman", "avalid22").then(async() => {
+        console.log("searching for " + manga_title);
+        await MFA.Manga.search({
+                    title: manga_title,
+                    limit: 6,
+                    offset: offset,
+                },
+                false
+            )
+            .then(async(list) => {
+                console.log("manga list found");
+                if (list.length === 0) {
+                    res.render("show_manga/show_manga.ejs", {
+                        error: "No manga found!",
+                        list: [],
+                    });
+                }
 
-        async function getCovers(things) {
-          const results = [];
-          for (const thing of things) {
-            // Good: all asynchronous operations are immediately started.
-            url = (await thing.mainCover.resolve()).image256;
-            base64 = imageToBase64(url);
-            results.push(base64);
-          }
+                async function getCovers(things) {
+                    const results = [];
+                    for (const thing of things) {
+                        // Good: all asynchronous operations are immediately started.
+                        url = await thing.mainCover.resolve();
+                        results.push(url);
+                    }
 
-          // Now that all the asynchronous operations are running, here we wait until they all complete.
-          return await Promise.all(results);
-        }
+                    // Now that all the asynchronous operations are running, here we wait until they all complete.
+                    return await Promise.all(results);
+                }
 
-        console.log("all ended :)");
+                console.log("all ended :)");
 
-        var url256 = await getCovers(list);
+                var url256 = await getCovers(list);
 
-        res.render("show_manga/show_manga.ejs", {
-          url: url256,
-          manga_title: manga_title,
-          list: list,
-          offset: offset,
-          error: "",
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  });
+                res.render("show_manga/show_manga.ejs", {
+                    url: url256,
+                    manga_title: manga_title,
+                    list: list,
+                    offset: offset,
+                    error: "",
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    });
 }
 
 exports.showManga = showManga;
