9c2b81132da655db418bbbc0585792b2c1356a56
ayman lyesri <lyesri99@gmail.com>
2022-06-06 16:14:04 +0100

    testing pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,24 +1,13 @@
 module.exports = {
     apps: [{
-            script: "./server/server.js",
-            watch: ".",
+        name: "app",
+        script: "./server/server.js",
+        instances: "max",
+        env: {
+            NODE_ENV: "development",
         },
-        {
-            script: "./service-worker/",
-            watch: ["./service-worker"],
+        env_production: {
+            NODE_ENV: "production",
         },
-    ],
-
-    deploy: {
-        production: {
-            user: "SSH_USERNAME",
-            host: "SSH_HOSTMACHINE",
-            ref: "origin/master",
-            repo: "GIT_REPOSITORY",
-            path: "DESTINATION_PATH",
-            "pre-deploy-local": "",
-            "post-deploy": "npm install && pm2 reload ecosystem.config.js --env production",
-            "pre-setup": "",
-        },
-    },
+    }, ],
 };
